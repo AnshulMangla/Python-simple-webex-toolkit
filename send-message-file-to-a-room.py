68325f062eb45cc7f27f360df5170c820bf85582
AnshulMangla <87487619+AnshulMangla@users.noreply.github.com>
2023-02-17 16:19:56 +1100

    Create send-message-file-to-a-room.py

---
+++ send-message-file-to-a-room.py
@@ -0,0 +1,17 @@
+from webexteamssdk import WebexTeamsAPI
+
+api = WebexTeamsAPI(access_token='[Insert Webex API Token]')
+
+
+rooms = api.rooms.list()
+
+for room in rooms:
+	if 'My Room' in room.title:
+		roomId = room.id
+		break
+    
+file_list = ["./myfile.txt"]
+
+api.messages.create(roomId, text='Hello World')
+
+api.messages.create(roomId=roomId, files=file_list)
